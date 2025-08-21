01cad2a095f941fa60ae2ef8ccf0ba172a494af4
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-21 13:12:08 +0530

    hello world
    printing hello world i created a fucntion in the void main and then i just called it inside the int main

---
+++ OOPS/oopslab1/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// A simple function to print "Hello, World!"
+void printHelloWorld()
+{
+  cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+  printHelloWorld(); // call the function
+  return 0;          // indicate program ended successfully
+}
